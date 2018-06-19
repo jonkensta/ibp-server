a2f3bc75c07a2d2476f74da86c0cadeaa69b17c8
Jonathan Starr] <jstarr@carnegietechnologies.com>
2018-06-18 22:33:09 -0500

    Use single-quotes for token-like strings

--- ibp/__init__.py
+++ ibp/__init__.py
@@ -87,11 +87,11 @@ csrf = CSRFProtect(app)
 
 db = SQLAlchemy(app)
 db.Model.metadata.naming_convention = {
-    "ix": 'ix_%(column_0_label)s',
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_%(constraint_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
+    'ix': 'ix_%(column_0_label)s',
+    'uq': 'uq_%(table_name)s_%(column_0_name)s',
+    'ck': 'ck_%(table_name)s_%(constraint_name)s',
+    'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+    'pk': 'pk_%(table_name)s'
 }
 
 app.config['REMEMBER_COOKIE_DURATION'] = timedelta(minutes=10)
