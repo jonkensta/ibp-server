9b955f70d37ad9550a7a40b860908c3694cbfc4b
Jonathan Starr <jstarr@carnegietechnologies.com>
2018-08-26 12:01:06 -0500

    Revert "Record and use names for users"
    This reverts commit c24adb6ece3f82ecdcfde8ad7e58cb5b096611b2.

--- ibp/flask_forms.py
+++ ibp/flask_forms.py
@@ -93,6 +93,15 @@ class Unit(FlaskForm):
 
 
 class Comment(FlaskForm):
+    author = fields.StringField(
+        'author', [
+            validators.InputRequired(),
+            validators.length(
+                min=1,
+                message="Please input your name"
+            )
+        ]
+    )
     comment = fields.StringField(
         'comment', [
             validators.InputRequired(),
