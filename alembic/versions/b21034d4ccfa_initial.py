d656a4f96dc6749193ed6a5445a9f49e3f08748a
Jonathan Starr <jonstarr67@gmail.com>
2018-10-13 14:29:55 -0500

    Initialize alembic versioning

---
+++ alembic/versions/b21034d4ccfa_initial.py
@@ -0,0 +1,24 @@
+"""Initial
+
+Revision ID: b21034d4ccfa
+Revises: 
+Create Date: 2018-10-07 01:30:03.261611
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'b21034d4ccfa'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
