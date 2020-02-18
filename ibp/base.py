67fe40e8f2814857114165a17d24680702834229
Jonathan Starr <jstarr@carnegietechnologies.com>
2020-02-18 13:16:47 -0600

    Load configuration files in base module

--- ibp/base.py
+++ ibp/base.py
@@ -1,5 +1,19 @@
+"""IBP server base module."""
+
+import configparser
+from os.path import abspath, dirname
+from os.path import join as join_path
+
 import bottle
+
+# pylint: disable=unused-import
 
 from . import models
 from . import routes
 from .routes import app
+
+# pylint: disable=invalid-name
+
+config_fpath = abspath(join_path(dirname(__file__), "..", "conf", "server.conf"))
+config = configparser.ConfigParser()
+config.read([config_fpath, ".ibpserver.conf"])
