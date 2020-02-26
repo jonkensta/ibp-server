5cb8fb1fb8ee59fc7e17c0d1bb086fefbecd1be7
Jonathan Starr <jstarr@carnegietechnologies.com>
2020-02-26 15:53:51 -0600

    Add route imports at the end to prevent loop

--- ibp/base.py
+++ ibp/base.py
@@ -4,16 +4,14 @@ import configparser
 from os.path import abspath, dirname
 from os.path import join as join_path
 
-import bottle
-
 # pylint: disable=unused-import
+
+config = configparser.ConfigParser()
+_config_fpath = abspath(join_path(dirname(__file__), "..", "conf", "server.conf"))
+config.read([_config_fpath, ".ibpserver.conf"])
 
 from . import models
 from . import routes
 from .routes import app
 
 # pylint: disable=invalid-name
-
-config_fpath = abspath(join_path(dirname(__file__), "..", "conf", "server.conf"))
-config = configparser.ConfigParser()
-config.read([config_fpath, ".ibpserver.conf"])
