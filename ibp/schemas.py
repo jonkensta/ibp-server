d9eb991812f8ecb4d742dec4d0783626cc03a1a0
Jonathan Starr <github@jstarr.me>
2023-09-04 16:49:44 -0500

    Switch schemas from `marshmallow` to `pydantic`

--- ibp/schemas.py
+++ ibp/schemas.py
@@ -1,85 +1,129 @@
-""":py:mod:`marshmallow` schemas for :py:mod:`ibp.models`.
+""":py:mod:`pydantic` schemas for :py:mod:`ibp.models`.
 
 The following schema classes and their corresponding instances are used in this
 project to serialize Python objects to and from JSON representations.
 
-The way that marshmallow works is that you can instantiate schema classes and
-use the resulting object to (de)serialize Python objects. In this module, we
-define a number of schema classes that correspond to the model classes defined
-in :py:mod:`ibp.models`. In addition, we also create a number of convenience
-instances of these classes that can be used directly without needing to
-instantiate anything.
-
-:note: See :py:mod:`marshmallow` for more details on marshalling.
+:note: See :py:mod:`pydantic` for more details on marshalling.
 
 """
 
-# pylint: disable=invalid-name, too-few-public-methods
+import datetime
+import enum
+import typing
 
-from marshmallow import Schema, fields, validate  # type: ignore
+import pydantic
+from pydantic import BaseModel
 
-from . import warnings
 
+class UnitBase(BaseModel):
+    """:py:mod:`pydantic` base schema for :py:class:`ibp.models.Unit`."""
 
-class UnitSchema(Schema):
-    """:py:mod:`marshmallow` schema for :py:class:`ibp.models.Unit`."""
+    name: pydantic.constr(to_upper=True)
+    """Unit name."""
 
-    id = fields.Int(dump_only=True)
-    """Read-only auto-incrementing unit index."""
-
-    name = fields.Str()
-    """Unit name encoded as a string."""
-
-    url = fields.URL()
+    url: pydantic.HttpUrl
     """Unit URL if available."""
 
 
-class LookupSchema(Schema):
-    """:py:mod:`marshmallow` schema for :py:class:`ibp.models.Lookup`."""
+class UnitCreate(UnitBase):
+    """:py:mod:`pydantic` create schema for :py:class:`ibp.models.Unit`."""
 
-    datetime = fields.DateTime(required=True)
+
+class Unit(UnitBase):
+    """:py:mod:`pydantic` schema for :py:class:`ibp.models.Unit`."""
+
+    id: int
+    """Read-only auto-incrementing unit index."""
+
+    class Config:
+        orm_mode = True
+
+
+class LookupBase(BaseModel):
+    """:py:mod:`pydantic` base schema for :py:class:`ibp.models.Lookup`."""
+
+
+class LookupCreate(LookupBase):
+    """:py:mod:`pydantic` create schema for :py:class:`ibp.models.Lookup`."""
+
+
+class Lookup(LookupBase):
+    """:py:mod:`pydantic` schema for :py:class:`ibp.models.Lookup`."""
+
+    index: int
+    """Read-only auto-incrementing unit index."""
+
+    datetime: datetime.datetime
     """Datetime of the volunteer lookup for an inmate."""
 
+    class Config:
+        orm_mode = True
 
-class CommentSchema(Schema):
-    """:py:mod:`marshmallow` schema for :py:class:`ibp.models.Comment`."""
 
-    index = fields.Int(dump_only=True)
-    """Read-only auto-incrementing comment index."""
+class CommentBase(BaseModel):
+    """:py:mod:`pydantic` base schema for :py:class:`ibp.models.Comment`."""
 
-    datetime = fields.DateTime(dump_only=True)
-    """Datetime of when the comment was made."""
-
-    author = fields.Str(validate=validate.Length(min=1), required=True)
+    author: pydantic.constr(min_length=1)
     """Author of the comment."""
 
-    body = fields.Str(validate=validate.Length(min=1), required=True)
+    body: pydantic.constr(min_length=1)
     """Body of the comment."""
 
 
-class RequestSchema(Schema):
-    """:py:mod:`marshmallow` schema for :py:class:`ibp.models.Request`."""
+class CommentCreate(BaseModel):
+    """:py:mod:`pydantic` create schema for :py:class:`ibp.models.Comment`."""
 
-    index = fields.Int(dump_only=True)
-    """Read-only auto-incrementing request index."""
 
-    date_postmarked = fields.Date(required=True)
+class Comment(BaseModel):
+    """:py:mod:`pydantic` schema for :py:class:`ibp.models.Comment`."""
+
+    index: int
+    """Read-only auto-incrementing comment index."""
+
+    datetime: datetime.datetime
+    """Datetime of when the comment was made."""
+
+    class Config:
+        orm_mode = True
+
+
+class RequestAction(str, enum.Enum):
+    """Enumeration of valid responses to a request."""
+
+    tossed = "Tossed"
+    filled = "Filled"
+
+
+class RequestBase(BaseModel):
+    """:py:mod:`pydantic` base schema for :py:class:`ibp.models.Request`."""
+
+    date_postmarked: datetime.date
     """USPS postmarkdate of the accompanying letter."""
 
-    action = fields.Str(validate=validate.OneOf(["Tossed", "Filled"]), required=True)
+    action: RequestAction
     """Action taken on the corresponding request."""
 
 
-class InmateSchema(Schema):
-    """:py:mod:`marshmallow` schema for :py:class:`ibp.models.Inmate`."""
+class Request(RequestBase):
+    """:py:mod:`pydantic` schema for :py:class:`ibp.models.Request`."""
 
-    jurisdiction = fields.Str()
+    index: int
+    """Read-only auto-incrementing request index."""
+
+    class Config:
+        orm_mode = True
+
+
+class InmateBase(BaseModel):
+    """:py:mod:`pydantic` base schema for :py:class:`ibp.models.Inmate`."""
+
+    jurisdiction: str
     """Prison system holding the inmate."""
 
-    id = fields.Int()
+    id: int
     """Inmate's numeric identifier as used in their jurisdiction."""
 
-    first_name = fields.Str()
+    first_name: str
     """Inmate first name.
 
     In some cases, this is given by the provider; in others cases, it is parsed
@@ -87,7 +131,7 @@ class InmateSchema(Schema):
 
     """
 
-    last_name = fields.Str()
+    last_name: str
     """Inmate last name.
 
     In some cases, this is given as-is by the provider; in others cases, it is
@@ -95,75 +139,78 @@ class InmateSchema(Schema):
 
     """
 
-    sex = fields.Str()
-    """Inmate gender as reported by provider."""
-
-    race = fields.Str()
-    """Inmate race as reported by provider."""
-
-    url = fields.URL()
-    """Inmate URL where their information is web accessible."""
-
-    release = fields.Str()
-    """Date of when this inmate is set to be released."""
-
-    release_warning = fields.Function(warnings.inmate_pending_release)
-    """Warning if an inmate is to be released soon."""
-
-    datetime_fetched = fields.Str()
-    """Datetime when inmate data was fetched from provider."""
-
-    entry_age_warning = fields.Function(warnings.inmate_entry_age)
-    """Warning if an inmate's information is stale."""
-
-    unit = fields.Nested(UnitSchema, only=["name", "url"])
+    unit: Unit
     """Prison unit holding the inmate."""
 
-    lookups = fields.Nested(LookupSchema, many=True)
+    sex: str
+    """Inmate gender as reported by provider."""
+
+    race: str
+    """Inmate race as reported by provider."""
+
+    url: typing.Optional[pydantic.HttpUrl]
+    """Inmate URL where their information is web accessible."""
+
+    release: str
+    """Date of when this inmate is set to be released."""
+
+
+class InmateCreate(InmateBase):
+    """:py:mod:`pydantic` create schema for :py:class:`ibp.models.Inmate`."""
+
+
+class Inmate(InmateBase):
+    """:py:mod:`pydantic` create schema for :py:class:`ibp.models.Inmate`."""
+
+    datetime_fetched: datetime.datetime
+    """Datetime when inmate data was fetched from provider."""
+
+    lookups: list[Lookup]
     """List of lookups performed on this inmate by IBP volunteers."""
 
-    comments = fields.Nested(CommentSchema, many=True)
+    comments: list[Comment]
     """List of comments on this inmate made by IBP volunteers."""
 
-    requests = fields.Nested(RequestSchema, many=True)
+    requests: list[Request]
     """List of requests made by this inmate."""
 
+    release_warning: str
+    """Warning if an inmate is to be released soon."""
 
-class ShipmentSchema(Schema):
-    """:py:mod:`marshmallow` schema for :py:class:`ibp.models.Shipment`."""
+    class Config:
+        orm_mode = True
 
-    date_shipped = fields.Date()
+
+class ShipmentBase(BaseModel):
+    """:py:mod:`pydantic` base schema for :py:class:`ibp.models.Shipment`."""
+
+    date_shipped: datetime.date
     """Date that the shipment was made."""
 
-    tracking_url = fields.Str()
+    tracking_url: pydantic.HttpUrl
     """Shipping service tracking URL if available."""
 
-    tracking_code = fields.Str()
+    tracking_code: str
     """Shipping service tracking code if available."""
 
-    weight = fields.Int()
+    weight: int
     """Weight of the shipment in ounces."""
 
-    postage = fields.Int()
+    postage: int
     """Postage of the shipment in US cents."""
 
 
-request = RequestSchema(unknown="EXCLUDE")
-"""Schema object for marshalling single :py:class:`ibp.models.Request` objects."""
+class ShipmentCreate(ShipmentBase):
+    """:py:mod:`pydantic` create schema for :py:class:`ibp.models.Shipment`."""
 
-comment = CommentSchema(unknown="EXCLUDE")
-"""Schema object for marshalling single :py:class:`ibp.models.Comment` objects."""
 
-inmate = InmateSchema()
-"""Schema object for marshalling single :py:class:`ibp.models.Inmate` objects."""
+class Shipment(ShipmentBase):
+    """:py:mod:`pydantic` schema for :py:class:`ibp.models.Shipment`."""
 
-shipment = ShipmentSchema(unknown="EXCLUDE")
-"""Schema object for marshalling single :py:class:`ibp.models.Shipment` objects."""
+    id: int
+    """Read-only auto-incrementing shipment index."""
 
-units = UnitSchema(many=True, only=["id", "name"])
-"""Schema object for marshalling multiple :py:class:`ibp.models.Unit` objects."""
+    class Config:
+        orm_mode = True
 
-inmates = InmateSchema(
-    many=True, only=["jurisdiction", "id", "first_name", "last_name", "unit.name"]
-)
-"""Schema object for marshalling multiple :py:class:`ibp.models.Inmate` objects."""
+
