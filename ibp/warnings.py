072df9d47a713476b9cfbbc685c2482ad92630fa
Jonathan Starr <github@jstarr.me>
2025-05-03 11:01:27 -0500

    warnings: fix `pylint` errors

--- ibp/warnings.py
+++ ibp/warnings.py
@@ -1,73 +1,82 @@
+"""Methods for generating warnings for inmates and requests."""
+
 import logging
 from datetime import date, datetime, timedelta
 
-import ibp
+from .base import config
 
 logger = logging.getLogger("warnings")
 
 
-def _inmate_entry_age_warning(inmate):
+def _inmate_entry_age_warning(inmate_):
+    """Return an entry age warning for an inmate if any."""
+    jurisdiction = inmate_.jurisdiction
+    id_ = inmate_.id
+
     try:
-        age = datetime.now() - inmate.datetime_fetched
+        age = datetime.now() - inmate_.datetime_fetched
     except TypeError:
-        msg = "Data entry for {} inmate #{:08d} has never been verified".format(
-            inmate.jurisdiction, inmate.id
-        )
+        msg = f"Data entry for {jurisdiction} inmate #{id_:08d} has never been verified"
         logger.debug(msg)
         return msg
 
-    inmates_cache_ttl = ibp.config.getint("warnings", "inmates_cache_ttl")
+    inmates_cache_ttl = config.getint("warnings", "inmates_cache_ttl")
     ttl = timedelta(hours=inmates_cache_ttl)
     if age > ttl:
-        msg = "Data entry for {} inmate #{:08d} is {} day(s) old".format(
-            inmate.jurisdiction, inmate.id, age.days
+        msg = (
+            f"Data entry for {jurisdiction} inmate #{id_:08d} is {age.days} day(s) old"
         )
         logger.debug(msg)
         return msg
-    else:
-        return None
+
+    return None
 
 
-def _inmate_release_warning(inmate):
+def _inmate_release_warning(inmate_):
+    """Return a release warning for an inmate if any."""
     try:
-        to_release = inmate.release - date.today()
+        to_release = inmate_.release - date.today()
     except TypeError:
         return None
 
-    days = ibp.config.getint("warnings", "min_release_timedelta")
+    days = config.getint("warnings", "min_release_timedelta")
     min_timedelta = timedelta(days=days)
 
+    jurisdiction = inmate_.jurisdiction
+    id_ = inmate_.id
+
     if to_release <= timedelta(0):
-        msg = "{} inmate #{:08d} is marked as released".format(
-            inmate.jurisdiction, inmate.id
+        msg = f"{jurisdiction} inmate #{id_:08d} is marked as released"
+        logger.debug(msg)
+        return msg
+
+    if to_release <= min_timedelta:
+        msg = (
+            f"{jurisdiction} inmate #{id_:08d} is {to_release.days} days from release."
         )
         logger.debug(msg)
         return msg
-    elif to_release <= min_timedelta:
-        msg = "{} inmate #{:08d} is {} days from release.".format(
-            inmate.jurisdiction, inmate.id, to_release.days
-        )
-        logger.debug(msg)
-        return msg
-    else:
-        return None
+
+    return None
 
 
-def inmate(inmate):
+def inmate(inmate_):
+    """Generate warnings about a given inmate."""
     messages = []
-    messages.append(_inmate_entry_age_warning(inmate))
-    messages.append(_inmate_release_warning(inmate))
+    messages.append(_inmate_entry_age_warning(inmate_))
+    messages.append(_inmate_release_warning(inmate_))
     messages = filter(None, messages)
     return messages
 
 
-def request(inmate, postmarkdate):
+def request(inmate_, postmarkdate):
+    """Generate warnings about a given request."""
     messages = []
 
-    def was_filled(request):
-        return request.action == "Filled"
+    def was_filled(request_):
+        return request_.action == "Filled"
 
-    requests = filter(was_filled, inmate.requests)
+    requests = filter(was_filled, inmate_.requests)
 
     try:
         last_filled_request = next(requests)
@@ -75,7 +84,7 @@ def request(inmate, postmarkdate):
         return messages
 
     td = postmarkdate - last_filled_request.date_postmarked
-    days = ibp.config.getint("warnings", "min_postmark_timedelta")
+    days = config.getint("warnings", "min_postmark_timedelta")
     min_td = timedelta(days=days)
 
     msg = None
@@ -87,7 +96,7 @@ def request(inmate, postmarkdate):
         msg = "No time has transpired since the last postmark."
         messages.append(msg)
     elif td < min_td:
-        msg = "Only {} days since last postmark.".format(td.days)
+        msg = f"Only {td.days} days since last postmark."
         messages.append(msg)
 
     if msg is not None:
